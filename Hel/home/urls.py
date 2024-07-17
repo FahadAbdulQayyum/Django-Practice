500531e12a558efb541cdd6695973cdb2403a650
Fahad <fahad.45854@iqra.edu.com>
2024-07-18 00:19:30 +0500

    admin credentials created

--- Hel/home/urls.py
+++ Hel/home/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 
+admin.site.site_header = "Fahad Custom Shop"
+admin.site.site_title = "Fahad Custom Shop"
+admin.site.index_title = "Welcome to FHD Custom Shop Portal"
+
 urlpatterns = [
     path("", views.index, name="home"),
     path("about", views.about, name="about"),
