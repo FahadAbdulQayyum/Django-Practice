bba1e2bd5af1e11acc3c2e889768363399a1f7f6
Fahad <fahad.45854@iqra.edu.com>
2024-07-18 00:01:19 +0500

    Django practices uploaded

---
+++ Hel/home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    # return HttpResponse("this is homepage")
+    context = {
+        'variable':"Fahad",
+        'year':"2"
+    }
+    return render(request,'index.html',context=context)
+    
+def about(request):
+    return HttpResponse("this is aboutpage")
+
+def contact(request):
+    return HttpResponse("this is contactpage")
+
+def services(request):
+    return HttpResponse("this is servicespage")
